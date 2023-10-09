d926f32ba4dc9f7541f3f80f4a2e42924a6b4d6b
FZenji <henmtol@gmail.com>
2023-10-09 14:22:26 -0400

    Add decrease button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -22,14 +22,28 @@ const ExpenseItem = (props) => {
             type: 'ADD_EXPENSE',
             payload: expense
         });
+    }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
     }
 
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><img src="https://pngimg.com/uploads/plus/plus_PNG121.png" alt="+" width="48" height="48" onClick={event=> increaseAllocation(props.name)}></img></td>
+        <td><img src="https://pngimg.com/uploads/minus/minus_PNG60.png" alt="-" width="48" height="48" onClick={event=> decreaseAllocation(props.name)}></img></td>
+        {/* <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td> */}
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
