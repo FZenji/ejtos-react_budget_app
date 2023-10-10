12d48b26233b18662101b757dc0fb0f335ee5d49
Henry Tolenaar <51287973+FZenji@users.noreply.github.com>
2023-10-10 05:59:08 +0100

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,9 +3,17 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
+        // if (event.target.value > 20000) {
+        //     event.target.value = 20000;
+        // }
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value
+        });
     }
     return (
 <div className='alert alert-secondary'>
